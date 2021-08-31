b4b7ddc31d32b7b5682d557aeba398ac4ee97f80
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2021-08-31 22:16:43 +0300

    Update and rename __template.cpp to main.cpp

---
+++ templates/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ar array
+
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define fi first
+#define se second
+
+#define FOR(i, n) for(int i=0; i<n; ++i)
+#define trav(a, x) for(auto &a : x) 
+
+const ll INF=1e18, M=1e9+7;
+const int mxN=1e6+5, mod=48;
+
+void solve() {
+  
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t=1;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
+} 
