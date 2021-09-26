a00358e4be094542f803d366115c36b10fb90f1a
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2021-09-26 23:57:34 +0300

    Update main.cpp

--- templates/main.cpp
+++ templates/main.cpp
@@ -9,26 +9,31 @@ using namespace std;
 #define pb push_back
 #define all(c) (c).begin(), (c).end()
 #define sz(x) (int)(x).size()
-#define mp make_pair
 #define fi first
 #define se second
 
 #define FOR(i, n) for(int i=0; i<n; ++i)
 #define trav(a, x) for(auto &a : x) 
 
-const ll INF=1e18, M=1e9+7;
-const int mxN=1e6+5, mod=48;
+const ll INF=2e18, mod=1e9+7, mxN=2e3;
+
+ll n, m, q, k, l, r, x, y, z;
+const ll template_array_size=1e6+37;
+ll a[template_array_size];
+ll b[template_array_size];
+ll c[template_array_size];
+string s, t;
 
 void solve() {
-  
+
 }
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int t=1;
-  cin >> t;
-  while(t--) {
+  int tc=1;
+  // cin >> tc;
+  while(tc--) {
     solve();
   }
-} 
+}
