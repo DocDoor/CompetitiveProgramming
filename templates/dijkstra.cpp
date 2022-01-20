4947c24c5be4da2c13b23decbaf2afaf8977f94f
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2022-01-20 23:07:54 +0300

    Update dijkstra.cpp

--- templates/dijkstra.cpp
+++ templates/dijkstra.cpp
@@ -1,20 +1,20 @@
-vector<pair<int, ll>> adj[200000]; // adjacency list
-priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq; // min heap
-vector<ll> dist(n+1, INF); // nodes , distances
+vector<pair<int, long long>> adj[200000]; // adjacency list
+priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq; // min heap
+vector<long long> dist(n + 1, 2e17); // nodes , distances
 
 // init edges 
 
-dist[1]=0;
+dist[1] = 0;
 pq.push({0, 1});
-while(pq.size()) {
-  auto [d, u]=pq.top(); pq.pop();
-  if(d>dist[u]) continue;
-  for(auto [v, w] : adj[u]) {
-    if(dist[v]>dist[u]+w) {
-      dist[v]=dist[u]+w;
+while (pq.size()) {
+  auto [d, u] = pq.top(); pq.pop();
+  if (d > dist[u]) continue;
+  for (auto [v, w] : adj[u]) {
+    if (dist[v] > dist[u] + w) {
+      dist[v] = dist[u] + w;
       pq.push({dist[v], v});
     }
   }
 }
 
-// for(int i=1; i<=n; ++i) cout << dist[i] << " ";
+// for (int i = 1; i <= n; ++i) cout << dist[i] << " ";
