07dc1c2bafddfc3c7c47fdc0d22158b2522ae0ec
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2022-01-20 23:01:40 +0300

    Update number_theory.cpp

--- templates/number_theory.cpp
+++ templates/number_theory.cpp
@@ -1,18 +1,18 @@
 namespace number_theory {  
-	ll gcd(ll x, ll y) {
-	  if(x==0) return y;
-	  if (y==0) return x;
-	  return gcd(y, x%y);
+	long long gcd(long long x, long long y) {
+	  if (x == 0) return y;
+	  if (y == 0) return x;
+	  return gcd(y, x % y);
 	}
 
-	bool isprime(ll n) { 
-	  if(n<=1) return false; 
-	  if(n<=3) return true; 
+	bool isprime(long long n) { 
+	  if(n <= 1) return false; 
+	  if(n <= 3) return true; 
 	 
-	  if(n%2==0||n%3==0) return false; 
+	  if (n % 2 == 0 || n % 3 == 0) return false; 
 	  
-	  for(ll i=5; i*i<=n; i+=6) {
-		  if(n%i==0||n%(i+2)==0) {
+	  for (long long i = 5; i * i <= n; i += 6) {
+		  if (n % i == 0 || n % (i + 2) == 0) {
 		    return false; 
       } 
     }
@@ -20,23 +20,23 @@ namespace number_theory {
 	  return true; 
 	} 
 	 
-	bool prime[15000105]; 
+	bool prime[1500105]; 
 	void sieve(int n) { 
-	  for(ll i=0; i<=n; ++i) prime[i]=1;
-	  for(ll p=2; p*p<=n; ++p) { 
-		  if(prime[p]==true) { 
-		    for(ll i=p*p; i<=n; i+=p) {
-			    prime[i]=false; 
+	  for (long long i = 0; i <= n; ++i) prime[i] = 1;
+	  for (long long p = 2; p * p <= n; ++p) { 
+		  if (prime[p] == true) { 
+		    for (long long i = p * p; i <= n; i += p) {
+			    prime[i] = false; 
         }
 		  } 
 	  } 
-	  prime[1]=prime[0]=0;
+	  prime[1] = prime[0] = 0;
 	} 
 	
-	vector<ll> getprimes(int n) {
-    vector<ll> primelist;
-	  sieve(n+1);
-	  for(ll i=2; i<=n; ++i) {
+	vector<long long> getprimes(int n) {
+    vector<long long> primelist;
+	  sieve(n + 1);
+	  for(long long i = 2; i <= n; ++i) {
       if(prime[i]) {
         primelist.push_back(i);
       }
@@ -44,12 +44,12 @@ namespace number_theory {
     return primelist;
 	}
 	
-	vector<ll> getdivs(ll n) {
-    vector<ll> divs;
-    for(ll i=1; i*i<=n; ++i) {
-      if(i*i==n) {
-        divs.pb(i);
-      } else if(n%i==0) {
+	vector<long long> getdivs(long long n) {
+    vector<long long> divs;
+    for (long long i = 1; i * i <= n; ++i) {
+      if (i * i == n) {
+        divs.push_back(i);
+      } else if (n % i == 0) {
         divs.push_back(i);
         divs.push_back(n/i);
       }
@@ -57,6 +57,6 @@ namespace number_theory {
     // maybe sort
     return divs;
   }
-}
+};
 
 using namespace number_theory;
