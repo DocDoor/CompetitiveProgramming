cb1cc2bb12cfbc79c5ed3d007f026dcdfc3da75a
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2022-01-20 23:06:15 +0300

    Update main.cpp

--- templates/main.cpp
+++ templates/main.cpp
@@ -12,13 +12,13 @@ using namespace std;
 #define fi first
 #define se second
 
-#define FOR(i, n) for(int i=0; i<n; ++i)
+#define FOR(i, n) for (int i = 0; i < n; ++i)
 #define trav(a, x) for(auto &a : x) 
 
 const ll INF=2e18, mod=1e9+7, mxN=2e3;
 
 ll n, m, q, k, l, r, x, y, z;
-const ll template_array_size=1e6+37;
+const ll template_array_size = 1e6 + 37;
 ll a[template_array_size];
 ll b[template_array_size];
 ll c[template_array_size];
@@ -31,9 +31,9 @@ void solve() {
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int tc=1;
+  int tc = 1;
   // cin >> tc;
-  while(tc--) {
+  while (tc--) {
     solve();
   }
 }
