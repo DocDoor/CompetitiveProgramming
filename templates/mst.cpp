e9fc27965e28d95bc406e90091609bf09c0807b8
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2022-01-20 23:03:21 +0300

    Update mst.cpp

--- templates/mst.cpp
+++ templates/mst.cpp
@@ -1,24 +1,31 @@
 struct DSU {
-  vector<int> sz, par;
+  vector<int> par, sz;
   DSU(int n) {
-    par.resize(n+1);
-    sz.resize(n+1, 1);
-    iota(all(par), 0);
+    par.resize(n);
+    sz.resize(n, 1);
+    iota(par.begin(), par.end(), 0);
   }
-  inline int find(int x) {
-    return par[x]=(x==par[x]?x:find(par[x]));
+  inline int get(int v) {
+    return par[v] = (v == par[v] ? v : get(par[v]));
   }
-  void unite(int x, int y) {
-    sz[x]+=sz[y];
-    par[y]=x;
+  inline bool unite(int x, int y) {
+    x = get(x);
+    y = get(y);
+    if (x == y) return false;
+    sz[x] += sz[y];
+    par[y] = x;
+    return true;
+  }
+  int size(int v) {
+    return sz[get(v)];
   }
   bool same(int x, int y) {
-    return find(x)!=find(y);
+    return get(x) == get(y);
   }
 };
 
 bool compare(pair<int, pair<int, int>> a, pair<int, pair<int, int>> b) {
-  return a.first<b.first;
+  return a.first < b.first;
 }
 
 /* sort edges by weights then iterate all edges and if u and v is in disjoint sets, add this edge into your tree */
