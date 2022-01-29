2fb5a40ab88421a95ab3bdf45af9f18c947ff50d
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2022-01-29 20:46:29 +0300

    Update abc218E.cpp

--- contests/AtCoder/abc218E.cpp
+++ contests/AtCoder/abc218E.cpp
@@ -25,47 +25,47 @@ const int mxN=2e5+5;
 struct DSU {
   vector<int> sz, par;
   DSU(int n) {
-    par.resize(n+1);
-    sz.resize(n+1, 1);
+    par.resize(n + 1);
+    sz.resize(n + 1, 1);
     iota(all(par), 0);
   }
   inline int find(int x) {
-    return par[x]=(x==par[x]?x:find(par[x]));
+    return par[x] = (x == par[x] ? x : find(par[x]));
   }
   bool unite(int x, int y) {
-    x=find(x); y=find(y);
-    if(x==y) return false;
-    par[x]=y;
+    x = find(x); y = find(y);
+    if (x == y) return false;
+    par[x] = y;
     return true;
   }
   bool same(int x, int y) {
-    return find(x)==find(y);
+    return find(x) == find(y);
   }
 };
 
 bool compare(pair<int, pair<int, int>> a, pair<int, pair<int, int>> b) {
-  return a.first<b.first;
+  return a.first < b.first;
 }
 
 void solve() {
   int n, m;
   cin >> n >> m;
-  DSU a(n+5);
+  DSU a(n + 5);
   vector<pair<ll, pair<ll, ll>>> edges;
-  FOR(i, m) {
+  FOR (i, m) {
     ll u, v, w;
     cin >> u >> v >> w;
     edges.pb({w, {u, v}});
   }
   sort(all(edges), compare);
-  ll ans=0;
+  ll ans = 0;
   trav(i, edges) {
-    if(i.fi<=0) {
+    if (i.fi <= 0) {
       a.unite(i.se.fi, i.se.se);
-    } else if(!a.same(i.se.fi, i.se.se)) {
+    } else if (!a.same(i.se.fi, i.se.se)) {
       a.unite(i.se.fi, i.se.se);
     } else {
-      ans+=i.fi;
+      ans += i.fi;
     }
   }
   cout << ans;
@@ -74,9 +74,9 @@ void solve() {
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int t=1;
+  int t = 1;
   // cin >> t;
-  while(t--) {
+  while (t--) {
     solve();
   }
 }
