5d8d178111d0393c96ea3d05661f21b39f1ba978
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2022-01-29 20:36:26 +0300

    Update LCA.cpp

--- templates/LCA.cpp
+++ templates/LCA.cpp
@@ -1,31 +1,31 @@
-const int mxN=2e5+3, k=25;
+const int mxN = 2e5 + 3, k = 25;
 vector<int> graph[mxN];
 int dist[mxN], anc[mxN][k];
  
-void dfs(long long src, long long p) {
-	par[src][0] = p;
-	for (long long i = 1; i < 25; ++i) {
-		par[src][i] = par[par[src][i - 1]][i - 1];
-	}
-	for (auto e : g[src]) {
-		if (e ^ p) {
-			depth[e] = depth[src] + 1;
-			dfs(e, src);
-		}
-	}
+void dfs(int src, int p) {
+  par[src][0] = p;
+  for (int i = 1; i < 25; ++i) {
+    par[src][i] = par[par[src][i - 1]][i - 1];
+  }
+  for (auto e : g[src]) {
+    if (e != p) {
+      depth[e] = depth[src] + 1;
+      dfs(e, src);
+    }
+  }
 }
 
  
-long long lca(int x, int y) {
-	if (depth[y] > depth[x]) swap(x, y);
- 	long long mesafe = depth[x] - depth[y], say = 0;
+int lca(int x, int y) {
+  if (depth[y] > depth[x]) swap(x, y);
+  long long mesafe = depth[x] - depth[y], say = 0;
   while (mesafe) {
     if (mesafe & 1) x = par[x][say];
     mesafe >>= 1;
     say++;
   }
   if (x == y) return x;
-  for (long long i = 24; i >= 0; --i) {
+  for (int i = 24; i >= 0; --i) {
     if (par[x][i] != par[y][i]) {
       x = par[x][i];
       y = par[y][i];
@@ -33,8 +33,6 @@ long long lca(int x, int y) {
   }
   return par[x][0];
 }
-
-
 
 // dist[1]=-1;
 // dfs(1, 1);
