f0973a0990dc71cf0edeb410d5ea8c6a8ffd9c15
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2022-01-29 20:31:22 +0300

    Update bit.cpp

--- templates/bit.cpp
+++ templates/bit.cpp
@@ -1,28 +1,26 @@
-
 struct ft {
-	vector<long long> bit;
-	int n;
+  vector<long long> bit;
+  int n;
+  ft(int x) {
+    n = x;
+    bit.resize(x + 1, 0);
+  }
 	
-	ft(int x) {
-		n = x;
-		bit.resize(x + 1, 0);
-	}
+  void add(int ind, int val) {
+    for (ind; ind <= n; ind += ind & -ind) {
+      bit[ind] += val;
+    }
+  }
 
-	void add(int ind, int val) {
-		for (ind; ind <= n; ind += ind & -ind) {
-			bit[ind] += val;
-		}
-	}
-
-	long long sum(int ind) {
-		long long ret = 0;
-		for (; ind > 0; ind -= ind & -ind) {
-			ret += bit[ind];
-		}
-		return ret;
-	}
-
-	long long query(int l, int r) {
-		return sum(r) - sum(l - 1);
-	}
+  long long sum(int ind) {
+    long long ret = 0;
+    for (; ind > 0; ind -= ind & -ind) {
+      ret += bit[ind];
+    }
+    return ret;
+  }
+	
+  long long query(int l, int r) {
+    return sum(r) - sum(l - 1);
+  }
 };
