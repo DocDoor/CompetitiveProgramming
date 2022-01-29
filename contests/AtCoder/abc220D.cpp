e45440c14a84ebceae09c251eac179286d3ecd4a
Mehmet Salih Erkan <68927156+DocDoor@users.noreply.github.com>
2022-01-29 20:44:00 +0300

    Update abc220D.cpp

--- contests/AtCoder/abc220D.cpp
+++ contests/AtCoder/abc220D.cpp
@@ -15,10 +15,10 @@ using namespace std;
 #define FOR(i, n) for(int i=0; i<n; ++i)
 #define trav(a, x) for(auto &a : x) 
 
-const ll INF=2e18, mod=998244353, mxN=2e5+5;
+const ll INF = 2e18, mod = 998244353, mxN = 2e5+5;
 
 ll n, m, q, k, l, r, x, y, z;
-const ll template_array_size=1e6+37;
+const ll template_array_size = 1e6+37;
 ll a[template_array_size];
 ll b[template_array_size];
 ll c[template_array_size];
@@ -28,17 +28,17 @@ ll dp[mxN][10];
 
 void solve() {
   cin >> n;
-  for(ll i=1; i<=n; ++i) cin >> a[i];
+  for (ll i = 1; i <= n; ++i) cin >> a[i];
   dp[1][a[1]]++;
-  for(ll i=1; i<=n-1; ++i) {
-    for(ll j=0; j<=9; ++j) {
-      dp[i+1][(j+a[i+1])%10]+=dp[i][j];
-      dp[i+1][(j*a[i+1])%10]+=dp[i][j];
-      dp[i+1][(j+a[i+1])%10]%=mod;
-      dp[i+1][(j*a[i+1])%10]%=mod;
+  for (ll i = 1; i <= n - 1; ++i) {
+    for (ll j = 0; j <= 9; ++j) {
+      dp[i + 1][(j + a[i + 1]) % 10] += dp[i][j];
+      dp[i + 1][(j * a[i + 1]) % 10] += dp[i][j];
+      dp[i + 1][(j + a[i + 1]) % 10] %= mod;
+      dp[i + 1][(j * a[i + 1]) % 10] %= mod;
     }
   }
-  for(int i=0; i<10; ++i) {
+  for (int i = 0; i < 10; ++i) {
     cout << dp[n][i] << '\n';
   }
 }
@@ -46,9 +46,9 @@ void solve() {
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  int tc=1;
+  int tc = 1;
   // cin >> tc;
-  while(tc--) {
+  while (tc--) {
     solve();
   }
 }
